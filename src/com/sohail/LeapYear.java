52c6a6a114896a68261d6e4de4544edea720ce5d
Sohailshah20 <contact.sohailali@gmail.com>
2022-05-13 12:32:50 +0530

    leap year problem

---
+++ src/com/sohail/LeapYear.java
@@ -0,0 +1,20 @@
+package com.sohail;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a year ");
+        int year = input.nextInt();
+//        boolean leapYear = ((year%4 == 0 && year%100 != 0) && (year%400 == 0 && year%100 == 0));
+        if (year%4 == 0 && year%100 != 0){
+            System.out.println("the year "+ year + " is a leap year");
+        }
+        else if (year%400 == 0 && year%100 == 0){
+            System.out.println("the year "+ year + " is a leap year");
+        } else {
+            System.out.println("the year "+ year + " is a not leap year");
+        }
+    }
+}
