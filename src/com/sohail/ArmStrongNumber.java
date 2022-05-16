faa1ce4eb6f7767aaece5dbaf48ecabfe2f89a19
Sohailshah20 <contact.sohailali@gmail.com>
2022-05-16 16:07:03 +0530

    armstrong problem

---
+++ src/com/sohail/ArmStrongNumber.java
@@ -0,0 +1,25 @@
+package com.sohail;
+
+import java.util.Scanner;
+
+public class ArmStrongNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = input.nextInt();
+        System.out.println(ArmStrong(number));
+    }
+
+    static boolean ArmStrong(int number) {
+        int original = number;
+        int sum = 0;
+        while (number > 0){
+            int rem = number%10;
+            int cube = rem * rem * rem;
+            sum += cube;
+            number = number/10;
+        }
+        return original == sum;
+
+    }
+}
